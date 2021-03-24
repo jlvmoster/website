5fc4e6141fa8c7035e792529203ad496123f9051
Jalo Moster <jlvmoster@gmail.com>
2021-03-24 11:24:02 -0400

    About profile card content updates

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,8 @@ import { StaticImage } from 'gatsby-plugin-image';
 const About = () => (
   <div className='mx-auto max-w-2xl bg-white shadow overflow-hidden sm:rounded-lg'>
     <div className='px-4 py-5 sm:p-6'>
-      <div className='space-y-4 sm:flex sm:space-x-6 sm:space-y-0'>
-        <div className='flex flex-shrink-0 justify-center sm:mb-0'>
+      <div className='sm:flex'>
+        <div className='flex flex-shrink-0 items-center justify-center sm:mb-0'>
           <StaticImage
             className='inline-block rounded-full'
             src='../images/professional.jpg'
@@ -17,11 +17,11 @@ const About = () => (
             width={180}
           />
         </div>
-        <div className='space-y-3'>
-          <h4 className='text-4xl font-medium font-thin'>Hi, I'm Jalo!</h4>
-          <p className='text-justify text-lg'>
-            I'm a Software Engineer with several years of experience in cloud and full-stack design and implementation.
-            I'm a motivated problem-solver with an aptitude for quickly mastering new skills, technologies, and roles.
+        <div className='mt-4 space-y-3 sm:ml-6 sm:mt-0'>
+          <h4 className='text-2xl font-medium font-thin sm:text-3xl'>Hi, I'm Jalo!</h4>
+          <p className='text-lg sm:text-justify sm:text-xl'>
+            Proud Georgia Tech Alumni and Software Engineer at AT&T. Experienced in designing cloud-native solutions
+            using modern full-stack technologies. Motivated problem-solver with an Agile mindset.
           </p>
         </div>
       </div>
