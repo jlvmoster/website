a8713c443e96cd8cf4ca739ebc78e2a83f0c8a7c
Jalo Moster <jlvmoster@gmail.com>
2021-03-31 18:31:20 -0400

    Footer content updates + version bump

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import GitHubIcon from '../icons/fontawesome/GitHub';
 
 const Footer = () => (
   <div className='z-50 bg-gray-800'>
     <div className='mx-auto px-2 max-w-7xl sm:px-6 lg:px-8'>
       <div className='flex items-center h-16'>
-        <h2 className='text-white font-mono text-xs'>
+        <h2 className='flex-grow text-white font-mono text-xs'>
           © {new Date().getFullYear()}, Built with{' '}
           <a
             className='hover:underline cursor-pointer'
@@ -14,6 +15,13 @@ const Footer = () => (
             Gatsby
           </a>
         </h2>
+        <a
+          className='w-7 h-7 text-gray-400 hover:text-white'
+          href='https://github.com/jlvmoster/website'
+          target='_blank'
+          rel='noreferrer'>
+          <GitHubIcon />
+        </a>
       </div>
     </div>
   </div>
